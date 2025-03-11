6052c07742eb5efc8b605eb665c9d202c9d34cef
gabija <gasta24@student.sdu.dk>
2025-03-11 10:52:45 +0100

    initializing Student class

---
+++ uniManagementApp/Models/Student.cs
@@ -0,0 +1,18 @@
+namespace uniManagementApp.Models;
+
+public class Student
+{
+    public int Id { get; private set; }
+    public string Name { get; set;}
+    public string Username { get; set;}
+    public string Password { get; set;}
+    public List<int> EnrolledSubjects { get; set; }
+
+    public Student(int id, string name, string username, string password)
+    {
+        Id = id;
+        Name = name;
+        Username = username;
+        Password = password;
+    }
+}
