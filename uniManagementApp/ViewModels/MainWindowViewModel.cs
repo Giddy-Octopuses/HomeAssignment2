cd4d87322407c05dd31882638f999fff34979109
AnnalenaLorenzen <lorenzenannalena@web.de>
2025-03-13 13:18:30 +0100

    navigation from login

--- uniManagementApp/ViewModels/MainWindowViewModel.cs
+++ uniManagementApp/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections.Generic;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 using uniManagementApp.Views;
+using uniManagementApp.Models;
 namespace uniManagementApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
@@ -14,23 +12,30 @@ public partial class MainWindowViewModel : ViewModelBase
     private UserControl currentView;
 
     private LoginView _loginView = new LoginView{DataContext = new LoginViewModel()};
-    private TeacherView _teacherView = new TeacherView{DataContext = new TeacherViewModel()};
-    private StudentView _studentView = new StudentView{DataContext = new StudentViewModel()};
+    private TeacherView _teacherView;
+    private StudentView _studentView;
 
     public MainWindowViewModel()
     {
         CurrentView = _loginView;
+        if (_loginView.DataContext is LoginViewModel loginViewModel)
+        {
+            loginViewModel.NavigateToStudentView = NavigateToStudentView;
+            loginViewModel.NavigateToTeacherView = NavigateToTeacherView;
+        }
     }
 
     [RelayCommand]
-    public void NavigateToTeacherView()
+    public void NavigateToTeacherView(Teacher teacher)
     {
+        _teacherView = new TeacherView{DataContext = new TeacherViewModel(teacher)};
         CurrentView = _teacherView;
     }
 
     [RelayCommand]
-    public void NavigateToStudentView()
+    public void NavigateToStudentView(Student student)
     {
+        _studentView = new StudentView{DataContext = new StudentViewModel(student)};
         CurrentView = _studentView;
     }
 }
