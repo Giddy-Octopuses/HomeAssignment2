c61d70b5011315d5540f6fff0cee21c90e1086be
semma7 <emmaj.solyom@gmail.com>
2025-03-16 18:13:00 +0100

    some progress

--- uniManagementApp/ViewModels/TeacherViewModel.cs
+++ uniManagementApp/ViewModels/TeacherViewModel.cs
@@ -1,57 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Avalonia.Controls;
 using uniManagementApp.Models;
-namespace uniManagementApp.ViewModels;
 
-public partial class TeacherViewModel : ViewModelBase
+namespace uniManagementApp.ViewModels
 {
-    public Teacher Teacher { get; }
-    public TeacherViewModel(Teacher teacher)
+
+    public partial class TeacherViewModel : ViewModelBase
     {
-        Teacher = teacher;
-    }
-    
-    // (?) private List<int> MySubjects = Models.Teacher.Subjects; 
-
-    public TeacherViewModel()
-    {
-        MySubjects = DataRepository.LoadData.Teachers(); // I'm not sure (how to load only the subjects of the specific teacher)
-    } // need to create datarepository in this class
-    // iterate through datarepository.Teachers -- find the correct teacher -- find the Subjects list
-
-    public ObservableCollection<Subject> MySubjects { get; } 
-
-    [ObservableProperty]
-    private string? newSubjectName;
-
-    [ObservableProperty]
-    private string? newSubjectDescription;
-
-    private Subject? _selectedSubject;
-    public Subject? SelectedSubject
-    {
-        get => _selectedSubject;
-        set => SetProperty(ref _selectedSubject, value);
-    }
-
-    [RelayCommand]
-    public void AddSubject()
-    {
-        if (string.IsNullOrWhiteSpace(NewSubjectName) || string.IsNullOrWhiteSpace(NewSubjectDescription))
+        public Teacher Teacher { get; }
+        public IDataRepository dataRepository = new DataRepository(); // need to create datarepository in this class
+        public TeacherViewModel(Teacher teacher)
         {
-            return;
+            Teacher = teacher;
+        }
+ 
+        /*    
+
+        [ObservableProperty]
+        private string? newSubjectName;
+
+        [ObservableProperty]
+        private string? newSubjectDescription; */
+
+        // I TRIED MAKING THE STACKPANEL VISIBLE ONLY AFTER A SUBJECT WAS CHOSEN, BUT IT DIDN'T WORK
+        /* private Subject? _selectedSubject;
+        public Subject? SelectedSubject
+        {
+            get => _selectedSubject;
+            set
+            {
+                if (SetProperty(ref _selectedSubject, value))
+                {
+                    IsSelected = (value != null);
+                    Console.WriteLine($"SelectedSubject: {SelectedSubject?.Name}, IsSelected: {IsSelected}");
+                }
+            }
         }
 
-        MySubjects.Add(new Subject { Name = NewSubjectName!, Description = NewSubjectDescription! });
-        NewSubjectName = NewSubjectDescription = null;
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set => SetProperty(ref _isSelected, value);
+        } */
+
+        private Subject? _selectedSubject;
+        public Subject? SelectedSubject
+        {
+            get => _selectedSubject;
+            set => SetProperty(ref _selectedSubject, value);
+        }  
+
+        /* [ObservableProperty]
+        private Subject? selectedSubject; */
+
+        /* 
+        [RelayCommand]
+        public void AddSubject()
+        {
+            if (string.IsNullOrWhiteSpace(NewSubjectName) || string.IsNullOrWhiteSpace(NewSubjectDescription))
+            {
+                return;
+            }
+
+            MySubjects.Add(new Subject { Name = NewSubjectName!, Description = NewSubjectDescription! });
+            NewSubjectName = NewSubjectDescription = null;
+        }
+
+        [RelayCommand] 
+        public void DeleteSubject()
+        {
+            if (SelectedItem != null)
+            {
+                MySubjects.Remove(SelectedItem);
+                // MISSING: also remove from AvailableSubjects for students
+                SelectedItem = null; // Clear the selection
+            }
+        } */
+    }
+    public class SubjectsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is List<int> subjects)
+            {
+                return string.Join(", ", subjects);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    [RelayCommand] 
-    public void DeleteSubject()
+    public class NullToBoolConverter : IValueConverter
     {
-        if (SelectedItem != null)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            MySubjects.Remove(SelectedItem);
-            // MISSING: also remove from AvailableSubjects for students
-            SelectedItem = null; // Clear the selection
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- uniManagementApp/Views/TeacherView.axaml.cs
+++ uniManagementApp/Views/TeacherView.axaml.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using uniManagementApp.Models;
+using uniManagementApp.ViewModels;
 
 namespace uniManagementApp.Views;
 
@@ -7,6 +11,13 @@ public partial class TeacherView : UserControl
     public TeacherView()
     {
         InitializeComponent();
-        DataContext = new TeacherViewModel();
-    }
+
+        //DataContext = new TeacherViewModel(new Teacher(1, "Mr. Smith", "smith123", "smith123", new List<int> { 1 }));
+    } 
+
+    /*
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    } */
 }
